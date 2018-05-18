b29c9857ffdccd2961d8f6c4adcfb457dd4682dc
Anthony Wrather <github@anthonywrather.me.uk>
2018-05-18 01:31:24 +0100

    Added JSON load

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http'
+import "rxjs/add/operator/map"
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http: Http, public navCtrl: NavController) {
 
   }
 
+  ionViewDidLoad() {
+    
+    this.http.get('/assets/data.json')
+    .map(res => res.json())
+    .subscribe(data => console.log(data));
+    
+    //this.http.get('people.json')
+      // Call map on the response observable to get the parsed people object
+    //.pipe(map(res => res.json()))
+      // Subscribe to the observable to get the parsed people object and attach it to the
+      // component
+    //.subscribe(people => this.people = people);
+
+  }
+  
 }
